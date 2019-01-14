17da17e7bb33119874f76a48600a1631957926ed
Agentvm <Agentvm@users.noreply.github.com>
2019-01-14 11:00:19 +0100

    added character (4 lines)

---
+++ Assets/Scripts/Objects/Character.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    int skill_spaceflight = 80;
+    int skill_navigation = 80;
+    int skill_mechanics = 50;
+    int skill_electronics = 50;
+}
